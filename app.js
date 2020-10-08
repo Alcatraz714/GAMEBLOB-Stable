838d3218561182670497cb4d5ee3bb91e199a35d
unknown <mihir.a.gupte@gmail.com>
2020-10-09 03:17:53 +0530

    converted home to ejs

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ app.get("/login", function(req,res){
 })
 
 app.get("/home", function(req,res){
-    res.send("This will be the home page")
+    const car_game = [
+        {img : "https://www.godisageek.com/wp-content/uploads/Doom-Eternal-Key-Art.jpg", desc : "Doom Eternal is a first-person shooter video game developed by id Software and published by Bethesda Softworks."},
+        {img : "https://cdn.mos.cms.futurecdn.net/GSYDS6PSPWPzWm3fFGj5gj-1200-80.jpg", desc: "No Man's Sky is an exploration real-time strategy survival game developed and published by the indie studio Hello Games." },
+        {img : "https://i.ytimg.com/vi/jEp9yyhwZFE/maxresdefault.jpg", desc : "Journey across a magical realm of diverse cultures and kingdoms in the epic title of Genshin Impact."}
+    ]
+    const all_game = [
+        {img : "https://ubistatic19-a.akamaihd.net/ubicomstatic/en-ca/global/game-info/acb_nakedbox_mobile_161339.jpg", title : "Assassins Creed Director's Cut", desc : "Assassin's Creed is an action-adventure video game developed by Ubisoft Montreal and published by Ubisoft."},
+        {img : "https://ubistatic19-a.akamaihd.net/ubicomstatic/en-us/global/game-info/acrogue_remastered-gameinfo-boxshot_art-ubinew-02-348x434_316967.jpg", title : "Assassin's Creed Rouge", desc : "Assassin's Creed Rogue an adventurous Assassin to a grim Templar willing to hunt down his former brothers."}
+    ]
+    res.render("home.ejs", {car_game: car_game, all_game: all_game})
 })
 
 app.get("/profile/:user", function(req,res){
